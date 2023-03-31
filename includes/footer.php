2fa15025289ce86649e81a67a697bfe3ddc87fb9
highoncodes1 <highoncodes@gmail.com>
2023-03-31 08:15:12 +0100

    message

---
+++ includes/footer.php
@@ -0,0 +1,133 @@
+<footer class="page-footer">
+
+	<div class="footer-nav">
+		<div class="container clearfix">
+
+			<div class="footer-nav__col footer-nav__col--info">
+				<div class="footer-nav__heading">Information</div>
+				<ul class="footer-nav__list">
+					<li class="footer-nav__item">
+						<a href="contact.php" class="footer-nav__link">Contact Us</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="about.php" class="footer-nav__link">About us</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="customer_register.php" class="footer-nav__link">Customer Registration</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="terms.php" class="footer-nav__link">Privacy &amp; cookies</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="cart.php" class="footer-nav__link">My Cart</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="footer-nav__col footer-nav__col--whybuy">
+				<div class="footer-nav__heading">Admin Panel</div>
+				<ul class="footer-nav__list">
+					<li class="footer-nav__item">
+						<a href="admin_area/dashboard.php" class="footer-nav__link">Admin Dashboard &amp; Login</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="/admin_area/edit_store.php" class="footer-nav__link">Manage Pages</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="/admin_area/index.php" class="footer-nav__link">Manage Database</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="/admin_area/customer_delete.php" class="footer-nav__link">Manage Customers</a>
+					</li>
+					<!-- <li class="footer-nav__item">
+						<a href="customer/pay_offline.php" class="footer-nav__link">Pay Offline</a>
+					</li> -->
+				</ul>
+			</div>
+
+			<div class="footer-nav__col footer-nav__col--account">
+				<div class="footer-nav__heading">Your account</div>
+				<ul class="footer-nav__list">
+					<li class="footer-nav__item">
+						<a href="customer/customer_login.php" class="footer-nav__link">Sign in</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="customer_register.php" class="footer-nav__link">Register</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="cart.php" class="footer-nav__link">View cart</a>
+					</li>
+					<!-- <li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">View your lookbook</a>
+					</li> -->
+					<li class="footer-nav__item">
+						<a href="order.php" class="footer-nav__link">Pay Offline</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="terms.php" class="footer-nav__link">Terms and Conditions</a>
+					</li>
+				</ul>
+			</div>
+
+
+			<div class="footer-nav__col footer-nav__col--contacts">
+				<div class="footer-nav__heading">Contact details</div>
+				<address class="address">
+				Head Office: Keffi.<br>
+				Nasarrawa, Nigeria.
+			</address>
+				<div class="phone">
+					Telephone:
+					<a class="phone__number" href="tel:+2348130979672">+2348130979672</a>
+				</div>
+				<div class="email">
+					Email:
+					<a href="mailto:bisi@gmail.com" class="email__addr">bisi@gmail.com</a>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="banners">
+		<div class="container clearfix">
+
+			<div class="banner-award">
+				<span>SHOP WITH US</span><br> @BISI NIGERIA
+			</div>
+
+			<div class="banner-social">
+				<a href="https://www.facebook.com" class="banner-social__link">
+				<i class="icon-facebook"></i>
+			</a>
+				<a href="#" class="banner-social__link">
+				<i class="icon-twitter"></i>
+			</a>
+				<a href="#" class="banner-social__link">
+				<i class="icon-instagram"></i>
+			</a>
+				<a href="#" class="banner-social__link">
+				<i class="icon-pinterest-circled"></i>
+			</a>
+			</div>
+
+		</div>
+	</div> 
+
+	<div class="page-footer__subline">
+		<div class="container clearfix">
+
+			<div class="copyright">
+				&copy; 2023 Bisi Nigeria &trade;
+			</div>
+
+			<!-- <div class="developer">
+				Dev by #TechBabyGirl
+			</div> -->
+
+			<div class="designby">
+			Designed and developed by #TechBabyGirl
+			</div>
+		</div>
+	</div>
+</footer>
